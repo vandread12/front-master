90c41de66100863323ba3b061ddbada1501fa811
vandread <mariobravo4@gmail.com>
2022-05-13 23:30:18 -0500

    tercer commit

--- functional1.js
+++ functional1.js
@@ -7,3 +7,8 @@ function RecursiveFibonacci(n){
     if(n===1) return 1;
     return RecursiveFibonacci(n-2) + RecursiveFibonacci(n-1)
 }
+
+function recursiveFactorial(n) {
+    if (n === 0) return 1;
+    return n * recursiveFactorial(n - 1);
+  }
